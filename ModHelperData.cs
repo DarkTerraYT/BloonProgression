706b819cbe77bd61ae8cc7b48bd4be4974e3c75b
CubedLeaves <105320711+DarkTerraYT@users.noreply.github.com>
2024-02-07 22:01:15 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -9,7 +9,7 @@ public static class ModHelperData
     public const string Description = "This mod Adds Four new Game Modes. Bloon Progression Easy, Bloon Progression Medium, Bloon Progression Hard, Bloon Progression Half Cash and the Hardest: Bloon Progression Impoppable"
         + "\n\nThe Bloon Progression is: Red - Blue - Green - Yellow - Pink - Black + White - Purple - Zebra - Lead (Only in Medium +  Hard) - Rainbow - Ceramic " +
         "- Moab - Bfb - Fortified Moab - Zomg - Fortified Bfb - Ddt - Fortified Zomg - Fortified Ddt - Bad - Fortified Bad. " +
-        "\n\nEasy Ends at Round 10, Medium Ends at Round 15 and Hard Ends at round 21. \n\n WARNING THIS MOD CAN BE REALLY HARD";
+        "\n\nEasy Ends at Round 10, Medium Ends at Round 15 and Hard Ends at round 21. \n\n WARNING THIS MOD CAN BE REALLY HARD. For any suggestions or questions contact me on discord: @lermcgerk";
 
     public const string RepoOwner = "DarkTerraYT"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "BloonProgression"; // TODO add your repo name here, also in the download url in README.md
